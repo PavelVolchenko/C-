20d094459e79ffefa6213cf86a286255817d8b9c
Pavel Volchenko <2793553@gmail.com>
2022-09-17 15:51:14 +0300

    Добавил восьмой пример -знакомство с массивами

---
+++ Example008_IntroArray/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[] array)
+{
+    int size = array.Length;
+    int index = 0;
+    while (index < size)
+    {
+        array[index] = new Random().Next(-100, 101);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    int index = 0;
+    while (index < size)
+    {
+        Console.Write($"{array[index]} ");
+        index++;
+    }
+}
+
+int Max(int[] array)
+{
+    int index = 1;
+    int size = array.Length;
+    int max = 0;
+    while (index < size)
+    {
+        if(array[index] > max)
+        {
+            max = array[index];
+        }
+        index++;
+    }
+    return max;
+}
+
+int[] arr = new int[20];
+
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+Console.Write($"Максимальное число: {Max(arr)}");
